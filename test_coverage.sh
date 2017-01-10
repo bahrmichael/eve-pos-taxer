03d994e48d85343291f88742b90a6c25290cf289
bahrmichael <mbahr92@gmail.com>
2017-01-10 18:45:55 +0100

    fixed coverage reporting so coveralls knows the branch

--- test_coverage.sh
+++ test_coverage.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # coverage and coveralls were put into this script as the execution of multiple commands connected with && failed
 coverage run -m unittest discover
-coveralls
+COVERALLS_REPO_TOKEN=WRZpqoMXoBs4z7exUYAQ6QFkjpnl5G78Z coveralls

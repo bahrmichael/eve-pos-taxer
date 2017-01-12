0cf7666cfd4719fa669b999f3d3b4e2b304cbf67
bahrmichael <mbahr92@gmail.com>
2017-01-13 00:24:59 +0100

    added code for error retrieveal

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,9 @@
+import json
+from pprint import pprint
+
 import discord
 import asyncio
+import requests
 
 client = discord.Client()
 
@@ -25,5 +29,13 @@ async def on_message(message):
     elif message.content.startswith('!sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
+    elif message.content.startswith('!errors'):
+        tmp = await client.send_message(message.channel, 'Reading the dark books ...')
+        response = requests.get('http://hordedelivery.com:11000/errors?authkey=7NbPOJCytgbI0KRGes6J')
+        json_data = json.loads(response.text)
+        corp_ids = set([])
+        for entry in json_data:
+            corp_ids.add(entry['corpId'])
+        await client.edit_message(tmp, 'CorpIds with error: %s' % json.dumps(list(corp_ids)))
 
 client.run('MjY5MjI1ODUxNzQzMjQwMTkz.C1mPqQ._s96H308u9FDL00fCqGWIB3z6kY')

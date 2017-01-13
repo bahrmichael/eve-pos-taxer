f6fd2dd7d37c763047f7f3886628dd55a0d3f6ca
bahrmichael <mbahr92@gmail.com>
2017-01-13 15:07:31 +0100

    pingBot can read errors and ping the notifiees

--- bot/pingBot.py
+++ bot/pingBot.py
@@ -1,24 +1,18 @@
-import json
-from pprint import pprint
+import os
 
 import discord
-import asyncio
 import requests
-import os
+import json
 
 client = discord.Client()
 
 
-async def my_background_task():
-    await client.wait_until_ready()
-    counter = 0
-    channel = discord.Object(id='general')
-    while not client.is_closed:
-        counter += 1
-        # await client.send_message(channel, counter)
-        print(counter)
-        await asyncio.sleep(2)  # task runs every 60 seconds
-
+def lambda_handler(event, context):
+    # message = event['Records'][0]['Sns']['Message']
+    # print("SNS Message: " + message)
+    # if message == "test-error":
+    client.run('MjY5MjI1ODUxNzQzMjQwMTkz.C1mPqQ._s96H308u9FDL00fCqGWIB3z6kY')
+    return "done"
 
 @client.event
 async def on_ready():
@@ -27,30 +21,52 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+    corp_ids = await find_corp_ids()
+
     channel_name = os.environ['CHANNEL']
     notifiees = os.environ['NOTIFIEES']
 
-    ch = None
-    for channel in client.get_all_channels():
-        if str(channel).lower() == channel_name.lower():
-            ch = channel
-            break
+    ch = await find_channel(channel_name)
 
     if ch is None:
         print("ERROR: Could not find channel " + channel_name)
         return
 
+    members = await find_members(notifiees)
+
+    if len(members) > 0 and len(corp_ids) >= 0:
+        target = members[0]
+        response = target.mention + ' CorpIds with error: %s' % json.dumps(list(corp_ids))
+        await client.send_message(ch, response)
+
+    await client.close()
+
+
+async def find_corp_ids():
+    response = requests.get('http://hordedelivery.com:11000/errors?authkey=7NbPOJCytgbI0KRGes6J')
+    json_data = json.loads(response.text)
+    corp_ids = set([])
+    for entry in json_data:
+        corp_ids.add(entry['corpId'])
+    return corp_ids
+
+
+async def find_members(notifiees):
     members = []
     for member in client.get_all_members():
         if str(member) in notifiees:
             members.append(member)
         else:
-            print("not in list " + str(member))
-
-    if len(members) > 0:
-        target = members[0]
-        response = target.mention + ", your message."
-        await client.send_message(ch, response)
+            print("%s is not in the list of notifiees" % str(member))
+    return members
 
 
-client.run('MjY5MjI1ODUxNzQzMjQwMTkz.C1mPqQ._s96H308u9FDL00fCqGWIB3z6kY')
+async def find_channel(channel_name):
+    ch = None
+    for channel in client.get_all_channels():
+        if str(channel) == channel_name:
+            ch = channel
+            break
+    return ch
+
+lambda_handler('ct', 'ct')

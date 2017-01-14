23ec6f33ff701d04b8d7dfbe35c4af8a002b9da6
bahrmichael <mbahr92@gmail.com>
2017-01-14 15:59:43 +0100

    forgot coverage report ..

--- test_coverage.sh
+++ test_coverage.sh
@@ -3,4 +3,4 @@
 git clone https://github.com/bahrmichael/eve-pos-taxer eve-pos-taxer
 cd eve-pos-taxer
 coverage run -m unittest discover
-#COVERALLS_REPO_TOKEN=WRZpqoMXoBs4z7exUYAQ6QFkjpnl5G78Z coveralls
+COVERALLS_REPO_TOKEN=WRZpqoMXoBs4z7exUYAQ6QFkjpnl5G78Z coveralls

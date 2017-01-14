9d19d4e34e7c9cd7974fd48e6f9245643d6aac16
bahrmichael <mbahr92@gmail.com>
2017-01-14 15:55:43 +0100

    updated coverage for parsers

--- test_coverage.sh
+++ test_coverage.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # coverage and coveralls were put into this script as the execution of multiple commands connected with && failed
+git clone https://github.com/bahrmichael/eve-pos-taxer eve-pos-taxer
+cd eve-pos-taxer
 coverage run -m unittest discover
-COVERALLS_REPO_TOKEN=WRZpqoMXoBs4z7exUYAQ6QFkjpnl5G78Z coveralls
+#COVERALLS_REPO_TOKEN=WRZpqoMXoBs4z7exUYAQ6QFkjpnl5G78Z coveralls
